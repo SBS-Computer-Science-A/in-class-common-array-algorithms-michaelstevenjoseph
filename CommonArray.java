5f1256faa84d6e998ca5fa109643539348d27f8a
MichaelfromSBS <Michael.Liu@sbs.org>
2024-01-09 14:54:50 -0500

    CommonArray

--- CommonArray.java
+++ CommonArray.java
@@ -1,6 +1,6 @@
 //Michael, Steven, Joseph
-public class CommonArray {
-    public static boolean even(int[] number){
+public class CommonArray {//The class
+    public static boolean even(int[] number){//go through the array and determine if there is odd number
         int pos=0;
         while(pos!=number.length){
             if(number[pos]%2==1){
@@ -10,7 +10,7 @@ public class CommonArray {
         }
         return true;
     }
-    public static void main(String[] args){
+    public static void main(String[] args){//main method testing the function
         int number[] = {2,4,6,8,10};
         System.out.println(even(number));
     }
